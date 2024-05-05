f2b5a1a56c2c489e0fc47000d67a5b9763489be5
Carlos Vinicios Martins Rocha <vinicios_martins2011@hotmail.com>
2024-05-04 22:44:00 -0300

    Update __init__.py

--- mmocr/models/common/losses/__init__.py
+++ mmocr/models/common/losses/__init__.py
@@ -1,4 +1,5 @@
-from .dice_loss import DiceLoss
-from .focal_loss import FocalLoss
+# from .dice_loss import DiceLoss
+# from .focal_loss import FocalLoss
 
-__all__ = ['DiceLoss', 'FocalLoss']
+# __all__ = ['DiceLoss', 'FocalLoss']
+__all__ = []
